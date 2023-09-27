f55db8a3a963b22985fb0beee138a8e3904f4ceb
root <hwcho@example.com>
2023-09-27 15:59:27 +0900

    최상위 path 변경

--- Board/src/main/java/com/study/domain/post/PostController.java
+++ Board/src/main/java/com/study/domain/post/PostController.java
@@ -35,7 +35,8 @@ public class PostController {
     }
 
     // 게시글 리스트 페이지
-    @GetMapping("/post/list.do")
+    //@GetMapping("/post/list.do")
+    @GetMapping("/")
     public String openPostList(@ModelAttribute("params") final SearchDto params, Model model) {
         PagingResponse<PostResponse> response = postService.findAllPost(params);
         model.addAttribute("response", response);
